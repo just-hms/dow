7941c944c30edc49bc0c39d217f53b0e2fc9e151
just-hms <alessandroversari1@gmail.com>
2024-03-26 13:35:43 +0100

    fix: move wasn't working with folders

--- osx/osx.go
+++ osx/osx.go
@@ -2,8 +2,6 @@ package osx
 
 import (
 	"errors"
-	"fmt"
-	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -24,35 +22,12 @@ func DownloadFolderPath() (string, error) {
 }
 
 func Move(sourcePath, destPath string) error {
-	inputFile, err := os.Open(sourcePath)
-	if err != nil {
-		return fmt.Errorf("couldn't open source file: %v", err)
-	}
-	defer inputFile.Close()
-
 	fInfo, err := os.Stat(destPath)
 	if err == nil && fInfo.IsDir() {
-		destPath = filepath.Join(destPath, filepath.Base(inputFile.Name()))
+		destPath = filepath.Join(destPath, filepath.Base(sourcePath))
 	}
 
-	outputFile, err := os.Create(destPath)
-	if err != nil {
-		return fmt.Errorf("couldn't open dest file: %v", err)
-	}
-	defer outputFile.Close()
-
-	_, err = io.Copy(outputFile, inputFile)
-	if err != nil {
-		return fmt.Errorf("couldn't copy to dest from source: %v", err)
-	}
-
-	inputFile.Close() // for Windows, close before trying to remove: https://stackoverflow.com/a/64943554/246801
-
-	err = os.Remove(sourcePath)
-	if err != nil {
-		return fmt.Errorf("couldn't remove source file: %v", err)
-	}
-	return nil
+	return os.Rename(sourcePath, destPath)
 }
 
 func LatestFile(files []fs.DirEntry) (os.FileInfo, error) {
