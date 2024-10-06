708cba8ca8e9b4ebf91f4da68bdb9de458886848
just-hms <alessandroversari1@gmail.com>
2024-10-06 22:59:08 +0200

    edit: another try on IsLocked function

--- pkg/osx/osx_windows.go
+++ pkg/osx/osx_windows.go
@@ -2,19 +2,14 @@ package osx
 
 import (
 	"os"
-	"syscall"
 )
-
-const ERROR_SHARING_VIOLATION = 32
 
 func IsLocked(path string) bool {
 	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
 		// If the error is a sharing violation, it means the file is in use
-		if pathErr, ok := err.(*os.PathError); ok {
-			if errno, ok := pathErr.Err.(syscall.Errno); ok && errno == ERROR_SHARING_VIOLATION {
-				return true
-			}
+		if _, ok := err.(*os.PathError); ok {
+			return true
 		}
 		return false
 	}
